61b40e0844700ed0134778d52c997efc4f2c6af0
Avinash Maricharla <avin9235@gmail.com>
2023-04-11 16:37:21 -0400

    lc_best_time_to_buy_and_sell_stock solution 1

--- array/lc_best_time_to_buy_and_sell_stock.ts
+++ array/lc_best_time_to_buy_and_sell_stock.ts
@@ -2,23 +2,27 @@
 
 
 
+
 /** Leetcode: 121.Best Time to Buy and Sell Stock */
 /** Solution 1: Using 'for loops' */
+/**
 let prices = [7, 1, 5, 3, 6, 4];
 
 var maxProfit = function (prices) {
     let profit = 0;
     for (let i = 0; i < prices.length; i++) {
-        const priceOfTheDay = prices[i];
+        const buyPrice = prices[i];
         for (let j = i + 1; j < prices.length; j++) {
-            const profitPrice = prices[j] - priceOfTheDay;
-            if (profitPrice > profit) {
-                profit = profitPrice;
+            const currentProfit = prices[j] - buyPrice; // consider prices[j] --> sell price
+            if (currentProfit > profit) {
+                profit = currentProfit;
             }
         }
     }
     return profit;
 };
+*/
+
 /**
 Notes:
     Time complexity: quadratic O(n^2) because of '2 for loops'.
@@ -29,10 +33,53 @@ Notes:
     4-7 = -3
  */
 /***************************** Solution 1: End *****************************/
+/** Solution 2: Using 'for loop' */
+let prices = [7, 1, 5, 3, 6, 4];
+
+var maxProfit = function (prices) {
+    let profit = 0;
+    let buyPrice = prices[0];
+    for (let i = 1; i < prices.length; i++) {
+        if (buyPrice > prices[i]) { // consider prices[i] --> sell price
+            buyPrice = prices[i];
+        }
+        const currentProfit = prices[i] - buyPrice;
+        if (currentProfit > profit) {
+            profit = currentProfit;
+        }
+    }
+    return profit;
+};
 
 /**
 Notes:
-    Other solutions you can work on
-    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/2573375/simple-javascript-solution/
-    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/39267/javascript-solution-if-anyone-is-interested/
+    Time complexity: linear-O(n) because of '1 for loop'.
+    Loop 1: 
+        stockToBuy 7 > 1 then stockToBuy = 1
+        currentProfit = 1 - 1; // 0
+        profit 0;
+    Loop 2:
+        stockToBuy 1 > 5 then stockToBuy = 1 // previous loop value
+        currentProfit = 5 - 1; // 4
+        profit 4;
+    Loop 3:
+        stockToBuy 1 > 3 then stockToBuy = 1 // previous loop value
+        currentProfit = 3 - 1; // 2
+        profit 4;
+    Loop 4:
+        stockToBuy 1 > 6 then stockToBuy = 1 // previous loop value
+        currentProfit = 6 - 1; // 5
+        profit 5;
+    Loop 5:
+        stockToBuy 1 > 4 then stockToBuy = 1 // previous loop value
+        currentProfit = 4 - 1; // 3
+        profit 5;
+
+    https://relatablecode.com/javascript-leetcode-best-time-to-buy-and-sell-stock
+ */
+/***************************** Solution 2: End *****************************/
+/**
+Notes:
+    Other solutions you can work on:
+    Using max and min: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/39267/javascript-solution-if-anyone-is-interested/
  */
